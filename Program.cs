f067529f237742af988bb62507287e705ab72942
«Svetlana» <timfr1209@gmail.com>
2022-12-13 01:34:11 +0300

    5 commit- solution

--- Program.cs
+++ Program.cs
@@ -2,3 +2,33 @@
 длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 */
+
+int[] RandomArray(int size, int minValue, int maxValue)
+{
+    int[] myArray = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        myArray[i] = new Random().Next(minValue, maxValue +1);
+    }
+    return myArray;
+}
+
+
+void ShowArray(int[] array)
+{
+    Console.WriteLine("Случайно заданный массив -> ");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+void ReducedArray(int[] array)
+{
+    Console.WriteLine("Получившийся массив -> ");
+    for(int i = 0; i <= 2; i++)
+    {
+       Console.Write(array[i] + " ");
+    }
+    
+}
